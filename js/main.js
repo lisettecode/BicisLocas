77b2018349b2ce0ad092486c7198d40cd39dd604
Liichy Aguilar Bueno <Liichy Aguilar Bueno>
2017-01-18 12:10:10 -0500

    Solución de Alertas

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function validateForm(){
-		/* Escribe tú código aquí */
-	var name = document.getElementById("name").value;
+		
+	
+    var name = document.getElementById("name").value;
 	if (name == ""){
 		alert("Ingresa tu nombre por favor.");
 	} else if (/^[a-z]/.test(name.charAt(0))){
@@ -8,18 +9,27 @@ function validateForm(){
 	} else if (/^[0-9]/.test(name.charAt(0))){
 		alert("Por favor no ingrese números.");
 	}
-	var lastname = document.getElementById("lastname").value;
+	
+   
+     var lastname = document.getElementById("lastname").value;
 	if (lastname == ""){
 		alert("Ingresa tu apellido por favor.");
+	} else if (/^[a-z]/.test(lastname.charAt(0))){
+		alert("Por favor ingrese la primera letra en mayúscula.");
+	} else if (/^[0-9]/.test(lastname.charAt(0))){
+		alert("Por favor no ingrese números.");
 	}
-	var correovalidado=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    
+	
+    var correovalidado=/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 	var email = document.getElementById("input-email").value;
 	if (email == "" ){
 		alert("Ingresa tu correo por favor.");
 	} else if(!correovalidado.test(email)) {
         alert("Ingrese un correo válido por favor.");
     }
-	var password = document.getElementById("input-password").value;
+	
+    var password = document.getElementById("input-password").value;
 	if (password == "") {
 		alert("Ingresa una contraseña por favor.");
 	} else if (password.length < 6){ 
@@ -27,10 +37,12 @@ function validateForm(){
     } else if (password == "123456" || password == "password" || password == "098754"){
     	alert("Ingrese una contraseña válida por favor.");
     }
+   
     var menu = document.querySelector("select").value;
 	if (menu == 0) {
 		alert("Seleccione un tipo de bici por favor");
-	}      
+	}  
+    
 }
 
 
